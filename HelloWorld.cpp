d6e1f019a2bdaee49bc48ff60f9b492310c0abe7
sktiwari21 <55376185+sktiwari21@users.noreply.github.com>
2019-09-16 02:55:42 -0400

    adding author

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Shubham Tiwari <gh9860@wayne.edu> 
 #include<iostream>  
   
 using namespace std; 
